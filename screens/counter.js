e7cf29379ea5db96d197901de3b61d14546a20db
NanaJuju08 <ana.105617@alunosatc.edu.br>
2025-04-02 17:13:36 -0300

    if do contador

--- screens/counter.js
+++ screens/counter.js
@@ -1,32 +1,69 @@
-import {Text, View, StyleSheet, ImageBackground, Image, Button, ScrollView} from 'react-native';
+import {Text, View, StyleSheet, ImageBackground, Image, Button, ScrollView, TouchableOpacity, TextInput} from 'react-native';
+import { useState} from 'react';
+import { TextInput } from 'react-native-gesture-handler';
+
 
 export default function Counter() {
-    return (
-        <View style={styles.container}>
-            <ImageBackground source={{uri: 'https://i.pinimg.com/474x/86/79/0b/86790b415c08b83e3919eca4cbe60c63.jpg'}} style={{flex: 1}}>
 
-                <View style={styles.texto}>
-                    <Text>CONTADOR</Text>
+    const [contador, setContador] = useState(0);
+
+    function Aumentar() {
+        setContador(contador + 1)
+    }
+    
+    function Diminuir() {
+        if (contador > 0) {
+            setContador (contador - 1)
+        }
+    }
+
+    return (
+        <ImageBackground source={{uri: 'https://i.pinimg.com/474x/86/79/0b/86790b415c08b83e3919eca4cbe60c63.jpg'}} style={{flex: 1}}>
+        <View style={styles.container}>
+                <Text style={styles.texto}>Contador: {contador}</Text>
+                <View style={styles.row}>
+                    <TouchableOpacity style={styles.btn} onPress={Aumentar}>
+                        <Text style={styles.txtbotao}> + </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.btn} onPress={Diminuir}>
+                        <Text style={styles.txtbotao}> - </Text>
+                    </TouchableOpacity>
+                    <TextInput style={styles.input} placeholder='E-mail'>
+                    </TextInput>
                 </View>
-                <Text>Contador: </Text>
-                <Button title='+' color={'#BE5985'}/>
-                <Button title='-' color={'#A7D477'}/>
-            </ImageBackground>
+
         </View>  
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex:1
+        flex:1,
+        alignItems: 'center',
+        justifyContent:'space-evenly'
     },
 
     texto: {
         color: '#BE5985',
-        fontSize: 25,
+        fontSize: 45,
         fontWeight: 'bold',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 10
+    },
+
+    row: {
+        width:'50%',
+        flexDirection:'row',
+        justifyContent:'space-around'
+    },
+
+    btn: {
+        backgroundColor: '#BE5985',
+        width: '40%',
+        borderRadius: 10,
+    },
+
+    txtbotao: {
+        textAlign: 'center',
+        color: '#ffff'
     }
 })
