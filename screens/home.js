0e662ac80a2c3bf51a57101bb9c9ed2375d945c9
NanaJuju08 <170452264+NanaJuju08@users.noreply.github.com>
2025-04-30 14:58:39 -0300

    firebase instalação

--- screens/home.js
+++ screens/home.js
@@ -70,7 +70,7 @@ const styles = StyleSheet.create({
     },
 
     textinho3: {
-        flex: 1,
+        flex: 2,
         justifyContent: 'center',
         alignItems: 'center',
         paddingStart:10
--- screens/products.js
+++ screens/products.js
@@ -5,9 +5,10 @@ import Card from '../components/cards';
 export default function Produtos(){
     const [produtos, setProdutos] = useState([
         {id: 1, nome: 'CAMISETA', valor: 9.99, imagem: 'https://saoko.com.br/cdn/shop/files/hanJisung.png?v=1701790877&width=1445'},
-        {id: 2, nome: 'MOLETOM', valor: 159.90, imagem: 'https://th.bing.com/th/id/OIP.7W3JmUjpY37V23c1OcDszQHaHa?rs=1&pid=ImgDetMain'},
-        {id: 3, nome: 'TÊNIS', valor: 89.90, imagem: 'https://i.pinimg.com/originals/45/2c/a7/452ca7d12e1c21656ee374818b93ed5f.jpg'},
-        {id: 4, nome: 'CALÇA', valor: 250.00, imagem: 'https://th.bing.com/th/id/OIP.7GCh1jnmP37Xj9y8tnnr4QHaLH?w=184&h=276&c=7&r=0&o=5&pid=1.7'}
+        {id: 2, nome: 'MOLETOM', valor: 159.94, imagem: 'https://th.bing.com/th/id/OIP.7W3JmUjpY37V23c1OcDszQHaHa?rs=1&pid=ImgDetMain'},
+        {id: 3, nome: 'TÊNIS', valor: 89.98, imagem: 'https://i.pinimg.com/originals/45/2c/a7/452ca7d12e1c21656ee374818b93ed5f.jpg'},
+        {id: 4, nome: 'CALÇA', valor: 250.35, imagem: 'https://th.bing.com/th/id/OIP.7GCh1jnmP37Xj9y8tnnr4QHaLH?w=184&h=276&c=7&r=0&o=5&pid=1.7'},
+        {id: 5, nome: 'MEIAS', valor: 20.65, imagem: 'https://pickles-home.com/data/1-17.jpg'}
     ])
     return(
         <View style={styles.container}>
