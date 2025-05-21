1e3f64d7d415c992fc1caa88dcd12369b466e48f
NanaJuju08 <170452264+NanaJuju08@users.noreply.github.com>
2025-05-21 16:59:47 -0300

    Só adicionando o $ que eu esqueci

--- screens/carrinho.js
+++ screens/carrinho.js
@@ -14,7 +14,7 @@ export default function Carrinho({navigation}){
                         <Image style={styles.imagens} source={{uri: item.imagem}} />
                         <View style={styles.view}>
                             <Text style={styles.textoProdutos}>{item.nome}</Text>  
-                            <Text style={styles.textoProdutos}>R{item.valor}</Text>  
+                            <Text style={styles.textoProdutos}>R${item.valor}</Text>  
                         </View>
                     </View>
                 )}
