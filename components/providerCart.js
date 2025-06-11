356945fe69b0a5bb7024dc268b4e380d3595f116
NanaJuju08 <170452264+NanaJuju08@users.noreply.github.com>
2025-06-11 14:48:22 -0300

    useEffects

--- components/providerCart.js
+++ components/providerCart.js
@@ -1,13 +1,69 @@
-import { createContext, useContext, useState } from "react";
-import {db, Auth} from '../controller';
+import { createContext, useContext, useEffect, useState } from "react";
+import {db, auth} from '../controller';
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
+import { ArraySchema } from "firebase/vertexai";
 
 const CarrinhoContext = createContext();
 
 export function ProviderCart({children}) {
 
     const [carrinho, setCarrinho] = useState([]);
+    const [usuario, setUsuario] = useState([]);
+    const [carregandoCarrinho, setCarregandoCarrinho] = useState(true);
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            setUsuario(user);
+            setCarregandoCarrinho(true);
+
+            if(user) {
+                try {
+                    const docRef = doc(db, 'carrinhos', user.uid);
+                    const docSnap = await getDoc(docRef);
+
+                    if(docSnap.exists()){
+                        const dados = docSnap.data();
+                        setCarrinho(Array.isArray(dados.produtos)? dados.produtos: [])
+                    }
+                    else {
+                        setCarrinho([]);
+                    }
+                }
+                catch (error) {
+                    console.log('Erro no carrinho', error);
+                    setCarrinho([]);
+                }
+            }
+
+            else {
+                setCarrinho([]);
+            }
+
+            setCarregandoCarrinho(false);
+        })
+        return() => unsubscribe();
+    }, []);
+
+    useEffect(() => {
+        async function salvaCarrinhoFirebase(lista) {
+            if(!usuario || carregandoCarrinho) {
+                return;
+            }
+            
+            try {
+                const docRef = doc(db, 'carrinhos'. usuario.uid);
+                await setDoc(docRef, {produtos:lista})
+            }
+
+            catch (error) {
+                console.log('Erro ao salvar no firebase', error)
+            }
+        }
+
+        salvaCarrinhoFirebase(carrinho);
+    }, [carrinho, usuario, carregandoCarrinho]);
 
     function AdicionarProdutos(produto){
         setCarrinho((anterior) => [...anterior, produto]) 
